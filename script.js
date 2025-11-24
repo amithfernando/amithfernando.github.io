da86269b56f87e520e70ea28ff2bc4a914e31bd8
Amith Fernando <amithishanka@hotmail.com>
2025-11-24 23:49:26 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Simple console greeting
+console.log("Welcome to my portfolio! Built with HTML, CSS, and JavaScript.");
